c227efa87d6a16a646981faa1ce3af724f8ec355
jorshi <jordieshier@gmail.com>
2017-08-12 01:49:43 -0600

    Working on PCA

--- Source/DimensionReduction.cpp
+++ Source/DimensionReduction.cpp
@@ -10,6 +10,13 @@
 
 #include "DimensionReduction.h"
 
+#include "tnt/tnt2vector.h"
+#include "tnt/jama_lu.h"
+#include "tnt/jama_eig.h"
+
+using namespace essentia;
+using namespace standard;
+
 DimensionReduction::DimensionReduction(const DBConnector& db, const ReferenceCountedArray<SampleFolder>& f) :
     Thread("Dimension Reduction Thread"), db_(db), sampleFolders_(f)
 {
@@ -20,6 +27,15 @@ DimensionReduction::DimensionReduction(const DBConnector& db, const ReferenceCou
     
     sampleClasses_.add(new SampleClassPCA(1, 0.2, 0.1));    // Kick Drum PCA Segmentations
     sampleClasses_.add(new SampleClassPCA(2, 0.5, 0.25));   // Snare Drum PCA Segmentations
+    
+    essentia::init();
+    AlgorithmFactory& factory = AlgorithmFactory::instance();
+    
+    pca_ = factory.create("PCA",
+                          "dimensions", 2,
+                          "namespaceIn", "feature",
+                          "namespaceOut", "pca");
+    
 }
 
 
@@ -60,11 +76,79 @@ void DimensionReduction::pca()
     {
         if (!threadShouldExit())
         {
-            String sql = "SELECT * FROM `samples` WHERE " \
-                "sample_type = " + String((*sampleClass)->sampleType) + \
-                " AND exclude = 0;";
+            // Get all samples for a sample type
+            String sql = "SELECT a.* FROM analysis a JOIN samples s ON a.sample_id = s.id " \
+                "WHERE s.sample_type = " + String((*sampleClass)->sampleType) + \
+                " AND s.exclude = 0;";
+
+            analysisMatrix_.clear();
+            sampleOrder_.clear();
+            db_.runCommand(sql, selectAnalysisPoolCallback, this);
             
-            std::cout << sql << "\n";
+            // Check to see if we need to exit the thread
+            if (threadShouldExit()) return;
+            
+            // how many dimensions are there?
+            int bands = analysisMatrix_[0].size();
+            
+            // calculate covariance for this songs frames
+            // before there was an implementation for covariance from Vincent akkerman. I (eaylon) think it is better
+            // and more maintainable to use an algorithm that computes covariance.
+            // Using singleGaussian algo seems to give slightly different results for variances (8 decimal places)
+            TNT::Array2D<Real> matrix, covMatrix, icov;
+            std::vector<Real> means;
+            matrix = vecvecToArray2D(analysisMatrix_);
+            Algorithm* sg = AlgorithmFactory::create("SingleGaussian");
+            sg->input("matrix").set(matrix);
+            sg->output("mean").set(means);
+            sg->output("covariance").set(covMatrix);
+            sg->output("inverseCovariance").set(icov);
+            sg->compute();
+            delete sg;
+            
+            // calculate eigenvectors, get the eigenvector matrix
+            JAMA::Eigenvalue<Real> eigMatrixCalc(covMatrix);
+            Array2D<Real>    eigMatrix;
+            eigMatrixCalc.getV(eigMatrix);
+            
+            int nFrames = analysisMatrix_.size();
+            for (int row=0; row<nFrames; row++) {
+                for (int col=0; col<bands; col++) {
+                    analysisMatrix_[row][col] -= means[col];
+                }
+            }
+            
+            // reduce dimensions of eigMatrix
+            int requiredDimensions = bands;
+            if (requiredDimensions > eigMatrix.dim2() || requiredDimensions < 1)
+                requiredDimensions = eigMatrix.dim2();
+            Array2D<Real> reducedEig(eigMatrix.dim1(), requiredDimensions);
+            
+            for (int row=0; row<eigMatrix.dim1(); row++) {
+                for (int column=0; column<requiredDimensions; column++) {
+                    reducedEig[row][column] = eigMatrix[row][column+eigMatrix.dim2()-requiredDimensions];
+                }
+            }
+            
+            // transform all the frames and add to the output
+            Array2D<Real> featVector(1,bands, 0.0);
+            vector<Real> results = vector<Real>(requiredDimensions, 0.0);
+            for (int row=0; row<nFrames; row++) {
+                for (int col=0; col<bands; col++) {
+                    analysisMatrix_[0][col] = analysisMatrix_[row][col];
+                }
+                featVector = matmult(featVector, reducedEig);
+                for (int i=0; i<requiredDimensions; i++) {
+                    results[i] = featVector[0][i];
+                }
+            }
+            
+            std::cout << "done";
+
+            
+
+            
+            
             
         }
     }
--- Source/DimensionReduction.h
+++ Source/DimensionReduction.h
@@ -13,11 +13,18 @@
 #include "../JuceLibraryCode/JuceHeader.h"
 #include <queue>
 
+#include <essentia/algorithmfactory.h>
+#include <essentia/essentiamath.h>
+#include <essentia/pool.h>
+
 #include "Sample.h"
 #include "SampleFolder.h"
 #include "dbConnector.h"
 #include "FeatureAnalysis.h"
 #include "AnalysisObject.h"
+
+
+
 
 class DimensionReduction : private Thread
 {
@@ -33,6 +40,16 @@ private:
     
     const DBConnector& db_;
     const ReferenceCountedArray<SampleFolder>& sampleFolders_;
+    ReferenceCountedArray<Sample> samples_;
+    ReferenceCountedArray<AnalysisObject> analyses_;
+    
+    // Essentia Pool object for storing matrix of analyses prior to PCA
+    std::vector<std::vector<Real>> analysisMatrix_;
+    std::vector<int> sampleOrder_;
+    Pool pcaIn_;
+    
+    // PCA Algorithm
+    Algorithm* pca_;
     
     // Struct for keeping track of the different sample types and the specific segmentation to use
     struct SampleClassPCA
@@ -47,8 +64,206 @@ private:
     OwnedArray<SampleClassPCA> sampleClasses_;
     
     void run() override;
-    
     void pca();
+    
+    
+    // Static callback for a select sample query
+    static int selectSampleCallback(void *param, int argc, char **argv, char **azCol)
+    {
+        DimensionReduction* parent = reinterpret_cast<DimensionReduction*>(param);
+        if (argc == 9)
+        {
+            Sample::Ptr newSample = new Sample(atoi(argv[0]),
+                                               String(CharPointer_UTF8(argv[1])),
+                                               String(CharPointer_UTF8(argv[2])),
+                                               atof(argv[3]),
+                                               atof(argv[4]),
+                                               atoi(argv[5]),
+                                               atoi(argv[6]),
+                                               atoi(argv[7]),
+                                               atoi(argv[8]));
+            
+            parent->samples_.add(newSample);
+        }
+        return 0;
+    }
+    
+    
+    static int selectAnalysisPoolCallback(void *param, int argc, char **argv, char **azCol)
+    {
+        DimensionReduction* parent = reinterpret_cast<DimensionReduction*>(param);
+        if (argc == 137)
+        {
+            parent->sampleOrder_.push_back(atoi(argv[1]));
+            std::vector<Real> newRow;
+            for (int i = 4; i < 137; i++)
+            {
+                newRow.push_back(atof(argv[i]));
+            }
+            parent->analysisMatrix_.push_back(newRow);
+        }
+        
+        return 0;
+    }
+    
+    
+    
+    static int selectAnalysisCallback(void *param, int argc, char **argv, char **azCol)
+    {
+        DimensionReduction* parent = reinterpret_cast<DimensionReduction*>(param);
+        if (argc == 137)
+        {
+            AnalysisObject::Ptr newAnalysis = new AnalysisObject(atoi(argv[0]),
+                                                                 atoi(argv[1]),
+                                                                 atof(argv[2]),
+                                                                 atof(argv[3]));
+            newAnalysis->bark_1_mean = atof(argv[4]);
+            newAnalysis->bark_2_mean = atof(argv[5]);
+            newAnalysis->bark_3_mean = atof(argv[6]);
+            newAnalysis->bark_4_mean = atof(argv[7]);
+            newAnalysis->bark_5_mean = atof(argv[8]);
+            newAnalysis->bark_6_mean = atof(argv[9]);
+            newAnalysis->bark_7_mean = atof(argv[10]);
+            newAnalysis->bark_8_mean = atof(argv[11]);
+            newAnalysis->bark_9_mean = atof(argv[12]);
+            newAnalysis->bark_10_mean = atof(argv[13]);
+            newAnalysis->bark_11_mean = atof(argv[14]);
+            newAnalysis->bark_12_mean = atof(argv[15]);
+            newAnalysis->bark_13_mean = atof(argv[16]);
+            newAnalysis->bark_14_mean = atof(argv[17]);
+            newAnalysis->bark_15_mean = atof(argv[18]);
+            newAnalysis->bark_16_mean = atof(argv[19]);
+            newAnalysis->bark_17_mean = atof(argv[20]);
+            newAnalysis->bark_18_mean = atof(argv[21]);
+            newAnalysis->bark_19_mean = atof(argv[22]);
+            newAnalysis->bark_20_mean = atof(argv[23]);
+            newAnalysis->bark_21_mean = atof(argv[24]);
+            newAnalysis->bark_22_mean = atof(argv[25]);
+            newAnalysis->bark_23_mean = atof(argv[26]);
+            newAnalysis->bark_24_mean = atof(argv[27]);
+            newAnalysis->bark_25_mean = atof(argv[28]);
+            newAnalysis->bark_26_mean = atof(argv[29]);
+            newAnalysis->bark_27_mean = atof(argv[30]);
+            
+            newAnalysis->bark_1_dev = atof(argv[31]);
+            newAnalysis->bark_2_dev = atof(argv[32]);
+            newAnalysis->bark_3_dev = atof(argv[33]);
+            newAnalysis->bark_4_dev = atof(argv[34]);
+            newAnalysis->bark_5_dev = atof(argv[35]);
+            newAnalysis->bark_6_dev = atof(argv[36]);
+            newAnalysis->bark_7_dev = atof(argv[37]);
+            newAnalysis->bark_8_dev = atof(argv[38]);
+            newAnalysis->bark_9_dev = atof(argv[39]);
+            newAnalysis->bark_10_dev = atof(argv[40]);
+            newAnalysis->bark_11_dev = atof(argv[41]);
+            newAnalysis->bark_12_dev = atof(argv[42]);
+            newAnalysis->bark_13_dev = atof(argv[43]);
+            newAnalysis->bark_14_dev = atof(argv[44]);
+            newAnalysis->bark_15_dev = atof(argv[45]);
+            newAnalysis->bark_16_dev = atof(argv[46]);
+            newAnalysis->bark_17_dev = atof(argv[47]);
+            newAnalysis->bark_18_dev = atof(argv[48]);
+            newAnalysis->bark_19_dev = atof(argv[49]);
+            newAnalysis->bark_20_dev = atof(argv[50]);
+            newAnalysis->bark_21_dev = atof(argv[51]);
+            newAnalysis->bark_22_dev = atof(argv[52]);
+            newAnalysis->bark_23_dev = atof(argv[53]);
+            newAnalysis->bark_24_dev = atof(argv[54]);
+            newAnalysis->bark_25_dev = atof(argv[55]);
+            newAnalysis->bark_26_dev = atof(argv[56]);
+            newAnalysis->bark_27_dev = atof(argv[57]);
+            
+            newAnalysis->bark_kurtosis = atof(argv[58]);
+            newAnalysis->bark_kurtosis_dev = atof(argv[59]);
+            newAnalysis->bark_skewness = atof(argv[60]);
+            newAnalysis->bark_skewness_dev = atof(argv[61]);
+            newAnalysis->bark_spread = atof(argv[62]);
+            newAnalysis->bark_spread_dev = atof(argv[63]);
+            
+            newAnalysis->mfcc_1_mean = atof(argv[64]);
+            newAnalysis->mfcc_2_mean = atof(argv[65]);
+            newAnalysis->mfcc_3_mean = atof(argv[66]);
+            newAnalysis->mfcc_4_mean = atof(argv[67]);
+            newAnalysis->mfcc_5_mean = atof(argv[68]);
+            newAnalysis->mfcc_6_mean = atof(argv[69]);
+            newAnalysis->mfcc_7_mean = atof(argv[70]);
+            newAnalysis->mfcc_8_mean = atof(argv[71]);
+            newAnalysis->mfcc_9_mean = atof(argv[72]);
+            newAnalysis->mfcc_10_mean = atof(argv[73]);
+            newAnalysis->mfcc_11_mean = atof(argv[74]);
+            newAnalysis->mfcc_12_mean = atof(argv[75]);
+            newAnalysis->mfcc_13_mean = atof(argv[76]);
+            
+            newAnalysis->mfcc_1_dev = atof(argv[77]);
+            newAnalysis->mfcc_2_dev = atof(argv[78]);
+            newAnalysis->mfcc_3_dev = atof(argv[79]);
+            newAnalysis->mfcc_4_dev = atof(argv[80]);
+            newAnalysis->mfcc_5_dev = atof(argv[81]);
+            newAnalysis->mfcc_6_dev = atof(argv[82]);
+            newAnalysis->mfcc_7_dev = atof(argv[83]);
+            newAnalysis->mfcc_8_dev = atof(argv[84]);
+            newAnalysis->mfcc_9_dev = atof(argv[85]);
+            newAnalysis->mfcc_10_dev = atof(argv[86]);
+            newAnalysis->mfcc_11_dev = atof(argv[87]);
+            newAnalysis->mfcc_12_dev = atof(argv[88]);
+            newAnalysis->mfcc_13_dev = atof(argv[89]);
+            
+            newAnalysis->hfc = atof(argv[90]);
+            newAnalysis->pitch_salience = atof(argv[91]);
+            newAnalysis->spectral_complexity = atof(argv[92]);
+            newAnalysis->spectral_crest= atof(argv[93]);
+            newAnalysis->spectral_decrease = atof(argv[94]);
+            newAnalysis->spectral_energy = atof(argv[95]);
+            newAnalysis->spectral_energyband_low = atof(argv[96]);
+            newAnalysis->spectral_energyband_middle_low = atof(argv[97]);
+            newAnalysis->spectral_energyband_middle_high = atof(argv[98]);
+            newAnalysis->spectral_energyband_high = atof(argv[99]);
+            newAnalysis->spectral_flatness_db = atof(argv[100]);
+            newAnalysis->spectral_flux = atof(argv[101]);
+            newAnalysis->spectral_rms = atof(argv[102]);
+            newAnalysis->spectral_rolloff = atof(argv[103]);
+            newAnalysis->spectral_strongpeak = atof(argv[104]);
+            
+            newAnalysis->hfc_dev = atof(argv[105]);
+            newAnalysis->pitch_salience_dev = atof(argv[106]);
+            newAnalysis->spectral_complexity_dev = atof(argv[107]);
+            newAnalysis->spectral_crest_dev = atof(argv[108]);
+            newAnalysis->spectral_decrease_dev = atof(argv[109]);
+            newAnalysis->spectral_energy_dev = atof(argv[110]);
+            newAnalysis->spectral_energyband_low_dev = atof(argv[111]);
+            newAnalysis->spectral_energyband_middle_low_dev = atof(argv[112]);
+            newAnalysis->spectral_energyband_middle_high_dev = atof(argv[113]);
+            newAnalysis->spectral_energyband_high_dev = atof(argv[114]);
+            newAnalysis->spectral_flatness_db_dev = atof(argv[115]);
+            newAnalysis->spectral_flux_dev = atof(argv[116]);
+            newAnalysis->spectral_rms_dev = atof(argv[117]);
+            newAnalysis->spectral_rolloff_dev = atof(argv[118]);
+            newAnalysis->spectral_strongpeak_dev = atof(argv[119]);
+            
+            newAnalysis->tristimulus_1 = atof(argv[120]);
+            newAnalysis->tristimulus_2 = atof(argv[121]);
+            newAnalysis->tristimulus_3 = atof(argv[122]);
+            newAnalysis->tristimulus_1_dev = atof(argv[123]);
+            newAnalysis->tristimulus_2_dev = atof(argv[124]);
+            newAnalysis->tristimulus_3_dev = atof(argv[125]);
+            
+            newAnalysis->spectral_centroid = atof(argv[126]);
+            newAnalysis->spectral_kurtosis = atof(argv[127]);
+            newAnalysis->spectral_skewness = atof(argv[128]);
+            newAnalysis->spectral_spread = atof(argv[129]);
+            newAnalysis->spectral_centroid_dev = atof(argv[130]);
+            newAnalysis->spectral_kurtosis_dev = atof(argv[131]);
+            newAnalysis->spectral_skewness_dev = atof(argv[132]);
+            newAnalysis->spectral_spread_dev = atof(argv[133]);
+            
+            newAnalysis->lat = atof(argv[134]);
+            newAnalysis->temporal_centroid = atof(argv[135]);
+            newAnalysis->rms = atof(argv[136]);
+            
+            parent->analyses_.add(newAnalysis);
+        }
+        return 0;
+    }
     
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(DimensionReduction)
     
